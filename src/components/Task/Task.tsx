9b2b6a8dd374a6f1ae367e764b48416dbf4a9d36
Hayeon Baek <bhy0512@gmail.com>
2025-11-19 01:24:29 +0900

    feat: Task Component

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,4 +1,24 @@
-const Task = () => {
-  return <div>Task</div>
-}
-export default Task
+import { container, description, title } from './Task.css';
+
+type TaskProps = {
+  index: number;
+  id: string;
+  boardId: string;
+  taskName: string;
+  taskDescription: string;
+};
+const Task: React.FC<TaskProps> = ({
+  index,
+  id,
+  boardId,
+  taskName,
+  taskDescription,
+}) => {
+  return (
+    <div className={container}>
+      <div className={title}>{taskName}</div>
+      <div className={description}>{taskDescription}</div>
+    </div>
+  );
+};
+export default Task;
