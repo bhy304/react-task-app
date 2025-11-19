c15a37ca73b2aea1ea0c8bc3cdc9fff739df21ba
Hayeon Baek <bhy0512@gmail.com>
2025-11-19 15:22:39 +0900

    feat: LoggerModal Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { useState } from 'react';
-import { appContainer, board, buttons } from './App.css';
+import {
+  appContainer,
+  board,
+  buttons,
+  deleteBoardButton,
+  loggerButton,
+} from './App.css';
 import BoardList from './components/BoardList/BoardList';
 import ListContainer from './components/ListContainer/ListContainer';
 import { useTypedSelector } from './hooks/redux';
 import EditModal from './components/EditModal/EditModal';
+import LoggerModal from './components/LoggerModal/LoggerModal';
 
 function App() {
+  const [isLoggerOpen, setIsLoggerOpen] = useState(false);
   const [activeBoardId, setActiveBoardId] = useState('board-0');
 
   const modalActive = useTypedSelector((state) => state.boards.modalActive);
@@ -18,6 +26,7 @@ function App() {
   return (
     <div className={appContainer}>
       {modalActive ? <EditModal /> : null}
+      {isLoggerOpen ? <LoggerModal setIsLoggerOpen={setIsLoggerOpen} /> : null}
       <BoardList
         activeBoardId={activeBoardId}
         setActiveBoardId={setActiveBoardId}
@@ -26,8 +35,12 @@ function App() {
         <ListContainer boardId={boardId} lists={lists} />
       </div>
       <div className={buttons}>
-        <button>이 게시판 삭제하기</button>
-        <button>활동 목록 보이기</button>
+        <button className={deleteBoardButton}>이 게시판 삭제하기</button>
+        <button
+          className={loggerButton}
+          onClick={() => setIsLoggerOpen(!isLoggerOpen)}>
+          {isLoggerOpen ? '활동 목록 숨기기' : '활동 목록 보이기'}
+        </button>
       </div>
     </div>
   );
--- src/components/LoggerModal/LoggerModal.tsx
+++ src/components/LoggerModal/LoggerModal.tsx
@@ -1,5 +1,38 @@
-const LoggerModal = () => {
-  return <div>LoggerModal</div>
-}
+import type React from 'react';
+import { useTypedSelector } from '../../hooks/redux';
+import { FiX } from 'react-icons/fi';
+import LogItem from './LogItem/LogItem';
+import {
+  body,
+  closeButton,
+  header,
+  modalContainer,
+  title,
+  wrapper,
+} from './LoggerModal.css';
 
-export default LoggerModal
+type LoggerModalProps = {
+  setIsLoggerOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const LoggerModal: React.FC<LoggerModalProps> = ({ setIsLoggerOpen }) => {
+  const logs = useTypedSelector((state) => state.logger.logArray);
+
+  return (
+    <div className={wrapper}>
+      <div className={modalContainer}>
+        <div className={header}>
+          <div className={title}>활동 기록</div>
+          <FiX className={closeButton} onClick={() => setIsLoggerOpen(false)} />
+        </div>
+        <div className={body}>
+          {logs.map((log) => (
+            <LogItem key={log.logId} logItem={log} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoggerModal;
